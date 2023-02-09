bbcccd2f6be3a97f9749a86ac0cfa0a9553d22d4
panvla <pandvlado@gmail.com>
2023-02-09 01:29:21 +0100

    added some projection interfaces and custom method in repository.

--- spring-data-rest-microservice/src/main/java/com/vladimirpandurov/springdatarestmicroservice/SpringDataRestMicroserviceApplication.java
+++ spring-data-rest-microservice/src/main/java/com/vladimirpandurov/springdatarestmicroservice/SpringDataRestMicroserviceApplication.java
@@ -10,8 +10,18 @@ import lombok.NoArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.data.rest.core.config.Projection;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringDataRestMicroserviceApplication {
@@ -19,7 +29,7 @@ public class SpringDataRestMicroserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataRestMicroserviceApplication.class, args);
 	}
-
+	@Bean
 	CommandLineRunner run (ProductRepository productRepository){
 		return args -> {
 			productRepository.save(new Product(null, "Lenovo", 10.00, true));
@@ -38,8 +48,11 @@ public class SpringDataRestMicroserviceApplication {
 	}
 
 }
+
 @Entity
-@Data @NoArgsConstructor @AllArgsConstructor
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 class Product{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -48,8 +61,39 @@ class Product{
 	private double price;
 	private boolean isInStock;
 }
-@Repository
-interface ProductRepository extends JpaRepository<Product, Long>{
-
+@RepositoryRestResource
+interface ProductRepository extends JpaRepository<Product, Long> {
+	@RestResource(path="/findByName")
+	Page<Product> findByNameContains(@Param("productName") String name, Pageable pageable);
 }
-
+@Projection(name = "web", types = Product.class)
+interface WebProjection{
+	Long getId();
+	String getName();
+	double getPrice();
+}
+@Projection(name="mobile", types = Product.class)
+interface MobileProjection {
+	Long getId();
+	String getName();
+}
+//@RestController
+//@AllArgsConstructor
+//class ProductController{
+//
+//	private final ProductRepository productRepository;
+//
+//	@GetMapping("/products")
+//	public List<Product> list(){
+//		return this.productRepository.findAll();
+//	}
+//	@GetMapping("/products/{id}")
+//	public Product getProduct(@PathVariable("id") Long id){
+//		return this.productRepository.findById(id).get();
+//	}
+//	@PostMapping(path = "/products")
+//	public Product saveProduct(@RequestBody Product product){
+//		return productRepository.save(product);
+//	}
+//
+//}
